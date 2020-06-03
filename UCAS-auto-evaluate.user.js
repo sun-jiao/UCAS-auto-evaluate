efc4ec4a3a2173a6b2981e37becb1141c9a5adf8
Sun Jiao <sunjiao@sunjiao.moe>
2020-06-03 15:42:35 +0800

    fix: item names changed.

--- UCAS-auto-evaluate.user.js
+++ UCAS-auto-evaluate.user.js
@@ -1,55 +1,62 @@
 // ==UserScript==
 // @name         UCAS auto evaluate
 // @namespace    https://www.zhihu.com/people/pinelliaternata/activities
-// @version      2.0
+// @version      3.0
 // @description  在中国科学院大学评教系统中自动选中“非常符合/非常满意”，并将文本框最小字数改为1个字。
 // @author       Sun Jiao
-// @match        http://jwxk.ucas.ac.cn/evaluate/*
+// @match        *://jwxk.ucas.ac.cn/evaluate/*
 // @grant        none
 // @license     description: CC BY-SA 4.0; code: GPL.
 // ==/UserScript==
 
-(function() {
+(function(){
     'use strict';
-    //document.body.innerHTML = document.body.innerHTML.replace(/value=\"5\" aria-required=\"true\">/g, 'value=\"5\" aria-required=\"true\" checked=\"true\">');
 
-    var single;
-    var i;
-    var item;
-
-    for (i = 2; i <= 87; i++) {
-        item = "item_" + i.toString();
-        single = document.getElementsByName(item);
-        if(single.length > 0){
-            single[0].checked = 1;
+    function choose(name){
+        var radio;
+        radio = document.getElementsByName(name);
+        if(radio.length > 0){
+            radio[0].checked = 1;
         }
+    };
+
+    function setlength(name){
+        var text;
+        text = document.getElementsByName(name);
+        if(text.length > 0){
+            text[0].setAttribute("minlength","1");
+        }
+    };
+
+    var i;
+
+    for (i = 2; i <= 159; i++) {
+        choose("item_" + i.toString());
     }
-    document.getElementsByName("radio_19")[1].checked = 1;
+
+    choose("radio_19");
+    choose("radio_102");
 
     var multi = document.getElementsByName("item_25");
     if(multi.length > 0){
-        for (i = 1; i <= 2; i++) {
+        for (i = 1; i <= 3; i++) {
             multi[i].checked = 1;
         }
     }
-    //document.body.innerHTML = document.body.innerHTML.replace(/minlength=\"15\"/g, 'minlength=\"1\"');
 
-    var text;
     for (i = 14; i <= 18; i++) {
-        item = "item_" + i.toString();
-        text = document.getElementsByName(item);
-        if(text.length > 0){
-            text[0].setAttribute("minlength","1");
-        }
+        setlength("item_" + i.toString());
     }
 
-    for (i = 43; i <= 44; i++) {
-        item = "item_" + i.toString();
-        text = document.getElementsByName(item);
-        if(text.length > 0){
-            text[0].setAttribute("minlength","1");
-        }
+    setlength("item_43");
+    setlength("item_44");
+
+    for (i = 97; i <= 101; i++) {
+        setlength("item_" + i.toString());
     }
+
+    setlength("item_145");
+    setlength("item_146");
 
     /*document.getElementsByName("item_14")[0].value = "授课老师风趣幽默而且干货多，能收获很多知识。";
     document.getElementsByName("item_15")[0].value = "我认为课堂上可以多一些互动交流。";
